0b2bc2e90ca6f8339c927c1a5e31beccdaccbc3c
rehamsaed <rsaed4214@gmail.com>
2025-07-09 00:20:14 +0300

    header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/Register/register.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', component:HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
 
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../../Components/header/header.component';
+
+@Component({
+  selector: 'app-home',
+  imports: [HeaderComponent],
+  templateUrl: './home.component.html',
+})
+export class HomeComponent {
+
+}
