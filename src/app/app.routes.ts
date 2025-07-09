c42437b19892ae4f6d1314932c66b6cab7d2fa7c
rehamsaed <rsaed4214@gmail.com>
2025-07-09 17:23:11 +0300

    Edit Profile

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/Register/register.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 export const routes: Routes = [
   { path: '', component:HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'profile', component: ProfileComponent },
 
 //   { path: '', redirectTo: 'login', pathMatch: 'full' },
  
